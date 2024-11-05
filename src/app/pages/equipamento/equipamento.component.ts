6ea551a189b998bda058c72810d2ef3bedd654ca
frederico.junior <frederico.junior@siriusgroup.com.br>
2024-11-05 11:39:48 -0300

    criaçaõ de páginas

---
+++ src/app/pages/equipamento/equipamento.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-equipamento',
+  standalone: true,
+  imports: [],
+  templateUrl: './equipamento.component.html',
+  styleUrl: './equipamento.component.scss'
+})
+export class EquipamentoComponent {
+
+}
---
+++ src/app/pages/recuperar-login/recuperar-login.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recuperar-login',
+  standalone: true,
+  imports: [],
+  templateUrl: './recuperar-login.component.html',
+  styleUrl: './recuperar-login.component.scss'
+})
+export class RecuperarLoginComponent {
+
+}

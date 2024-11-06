fda5945033737d3dcfee8a9253bad0fc1c1c5569
frederico.junior <frederico.junior@siriusgroup.com.br>
2024-11-06 10:51:09 -0300

    configurações iniciais do menu

--- src/app/pages/menu/menu.component.ts
+++ src/app/pages/menu/menu.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { MenubarModule } from 'primeng/menubar';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
-  standalone: true,
-  imports: [],
   templateUrl: './menu.component.html',
-  styleUrl: './menu.component.scss'
+  styleUrls: ['./menu.component.scss'],
+  standalone: true,
+  imports: [MenubarModule, CommonModule],
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit {
+  items: MenuItem[] | undefined;
 
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.items = [
+      {
+        icon: 'pi pi-align-justify',
+        items: [
+          {
+            label: 'Equipamento',
+            icon: 'pi pi-cog',
+            route: '/equipamento',
+          },
+          {
+            label: 'Login',
+            icon: 'pi pi-sign-in',
+            route: '/login',
+          },
+          {
+            label: 'Recuperar Login',
+            icon: 'pi pi-key',
+            route: '/recuperar-login',
+          },
+          {
+            label: 'Relatório',
+            icon: 'pi pi-file',
+            route: '/relatorio',
+          },
+        ],
+      },
+    ];
+  }
 }

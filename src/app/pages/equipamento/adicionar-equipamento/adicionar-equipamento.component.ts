e7c31dcc163caf35145a2bf81b7669e74246d0f1
frederico.junior <frederico.junior@siriusgroup.com.br>
2024-11-18 19:00:27 -0300

    configurações iniciais crud equipamento

---
+++ src/app/pages/equipamento/adicionar-equipamento/adicionar-equipamento.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-adicionar-equipamento',
+  standalone: true,
+  imports: [],
+  templateUrl: './adicionar-equipamento.component.html',
+  styleUrl: './adicionar-equipamento.component.scss'
+})
+export class AdicionarEquipamentoComponent {
+
+}
--- src/app/pages/equipamento/equipamento.component.ts
+++ src/app/pages/equipamento/equipamento.component.ts
@@ -2,8 +2,8 @@ import { Component } from "@angular/core";
 import { TableModule } from "primeng/table";
 import { MenuComponent } from "../menu/menu.component";
 import { CardModule } from "primeng/card";
-import { ButtonModule } from 'primeng/button';
-import { EquipamentoService } from '../../services/equipamento.service';
+import { ButtonModule } from "primeng/button";
+import { EquipamentoService } from "../../services/equipamento.service";
 
 interface Item {
   id: number;
@@ -15,16 +15,20 @@ interface Item {
 @Component({
   selector: "app-equipamento",
   standalone: true,
-  imports: [TableModule, CardModule, MenuComponent,ButtonModule],
+  imports: [TableModule, CardModule, MenuComponent, ButtonModule],
   templateUrl: "./equipamento.component.html",
   styleUrl: "./equipamento.component.scss",
 })
 export class EquipamentoComponent {
-  dados: Item[] = [];
+  dados: Item[] = [
+    { id: 1, number: "Equipamento 1", ownership: "Origem 1", qrCode: "QR1" },
+    { id: 2, number: "Equipamento 2", ownership: "Origem 2", qrCode: "QR2" },
+    { id: 3, number: "Equipamento 3", ownership: "Origem 3", qrCode: "QR3" },
+    { id: 4, number: "Equipamento 4", ownership: "Origem 4", qrCode: "QR4" },
+    { id: 5, number: "Equipamento 5", ownership: "Origem 5", qrCode: "QR5" },
+  ];
 
-  constructor(
-    private service: EquipamentoService
-  ) {}
+  constructor(private service: EquipamentoService) {}
 
   ngOnInit() {
     this.service.list().subscribe(
@@ -35,5 +39,15 @@ export class EquipamentoComponent {
         console.error(error);
       }
     );
+  }
+
+  editItem(item: Item) {
+    // Lógica para editar o item
+    console.log('Edit item:', item);
+  }
+
+  deleteItem(item: Item) {
+    // Lógica para excluir o item
+    console.log('Delete item:', item);
   }
 }

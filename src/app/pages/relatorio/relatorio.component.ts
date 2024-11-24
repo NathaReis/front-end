b620496837060b274291f685f0d8fdfc16748bd4
frederico.junior <frederico.junior@siriusgroup.com.br>
2024-11-24 14:20:52 -0300

    finalização página de relatório

--- src/app/pages/relatorio/relatorio.component.ts
+++ src/app/pages/relatorio/relatorio.component.ts
@@ -1,13 +1,132 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { CommonModule } from "@angular/common";
+import { TableModule } from "primeng/table";
 import { MenuComponent } from "../menu/menu.component";
+import { CardModule } from "primeng/card";
+import { ButtonModule } from "primeng/button";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { DialogModule } from "primeng/dialog";
+import { InputTextModule } from "primeng/inputtext";
+import { RegistroService } from "../../services/registro.service";
+import { ConfirmationService, MessageService } from "primeng/api";
+import { ToastModule } from "primeng/toast";
+import { ConfirmDialogModule } from "primeng/confirmdialog";
+import { DropdownModule } from 'primeng/dropdown';
+import { CalendarModule } from 'primeng/calendar';
+import { CheckboxModule } from 'primeng/checkbox';
 
 @Component({
   selector: 'app-relatorio',
   standalone: true,
-  imports: [MenuComponent],
+  imports: [
+    TableModule,
+    CardModule,
+    CommonModule,
+    MenuComponent,
+    ButtonModule,
+    DialogModule,
+    FormsModule,
+    ReactiveFormsModule,
+    InputTextModule,
+    ToastModule,
+    ConfirmDialogModule,
+    DropdownModule,
+    CalendarModule,
+    CheckboxModule,
+  ],
   templateUrl: './relatorio.component.html',
-  styleUrl: './relatorio.component.scss'
+  styleUrls: ['./relatorio.component.scss'],
+  providers: [ConfirmationService, MessageService],
 })
 export class RelatorioComponent {
+  relatorioForm: FormGroup;
 
+  constructor(
+    private fb: FormBuilder,
+    private registroService: RegistroService,
+    private messageService: MessageService
+  ) {
+    this.relatorioForm = this.fb.group({
+      startDate: [null, Validators.required],
+      endDate: [null, Validators.required],
+      historico: [true],
+      custos: [true],
+      status: [true],
+    });
+  }
+
+  onSubmit() {
+    if (this.relatorioForm.valid) {
+      const formData = this.relatorioForm.value;
+      console.log('Form Data:', formData);
+      this.messageService.add({
+        severity: 'success',
+        summary: 'Sucesso',
+        detail: 'Relatório gerado com sucesso',
+      });
+    } else {
+      this.messageService.add({
+        severity: 'error',
+        summary: 'Erro',
+        detail: 'Por favor, preencha todos os campos obrigatórios.',
+      });
+    }
+  }
+
+  onCancel() {
+    this.relatorioForm.reset();
+  }
+
+  downloadExcelReport() {
+    this.registroService.downloadExcelReport().subscribe(
+      (response) => {
+        const url = window.URL.createObjectURL(response);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = 'equipament_report.xlsx';
+        a.click();
+        window.URL.revokeObjectURL(url);
+        this.messageService.add({
+          severity: 'success',
+          summary: 'Sucesso',
+          detail: 'Relatório Excel baixado com sucesso',
+        });
+      },
+      (error) => {
+        console.error(error);
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Erro',
+          detail: 'Erro ao baixar relatório Excel',
+        });
+      }
+    );
+  }
+
+  downloadPdfReport() {
+    this.registroService.downloadPdfReport().subscribe(
+      (response) => {
+        const url = window.URL.createObjectURL(response);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = 'equipament_report.pdf';
+        a.click();
+        window.URL.revokeObjectURL(url);
+        this.messageService.add({
+          severity: 'success',
+          summary: 'Sucesso',
+          detail: 'Relatório PDF baixado com sucesso',
+        });
+      },
+      (error) => {
+        console.error(error);
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Erro',
+          detail: 'Erro ao baixar relatório PDF',
+        });
+      }
+    );
+  }
 }
---
+++ src/app/services/registro.service.ts
@@ -0,0 +1,82 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpHeaders,
+} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, switchMap } from 'rxjs/operators';
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RegistroService {
+  private apiUrl = 'https://devterrasa.com/java/v1/private/reports/';
+
+  constructor(private http: HttpClient, private authService: AuthService) {}
+
+  private getHeaders(): HttpHeaders {
+    const accessToken = localStorage.getItem('accessToken');
+    return new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${accessToken}`,
+    });
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    let errorMessage = 'Ocorreu um erro desconhecido.';
+    if (error.error instanceof ErrorEvent) {
+      errorMessage = `Erro: ${error.error.message}`;
+    } else {
+      errorMessage = `Erro ${error.status}: ${error.message}`;
+    }
+    console.error('An error occurred:', errorMessage);
+    return throwError(errorMessage);
+  }
+
+  private handle401Error(request: () => Observable<any>): Observable<any> {
+    return this.authService.refreshToken().pipe(
+      switchMap((tokens: any) => {
+        localStorage.setItem('accessToken', tokens.accessToken);
+        localStorage.setItem('refreshToken', tokens.refreshToken);
+        return request();
+      }),
+      catchError(this.handleError)
+    );
+  }
+
+  downloadExcelReport(): Observable<Blob> {
+    return this.http
+      .get(`${this.apiUrl}excel`, {
+        headers: this.getHeaders(),
+        responseType: 'blob',
+      })
+      .pipe(
+        catchError((error) => {
+          if (error.status === 401) {
+            return this.handle401Error(() => this.downloadExcelReport());
+          } else {
+            return this.handleError(error);
+          }
+        })
+      );
+  }
+
+  downloadPdfReport(): Observable<Blob> {
+    return this.http
+      .get(`${this.apiUrl}pdf`, {
+        headers: this.getHeaders(),
+        responseType: 'blob',
+      })
+      .pipe(
+        catchError((error) => {
+          if (error.status === 401) {
+            return this.handle401Error(() => this.downloadPdfReport());
+          } else {
+            return this.handleError(error);
+          }
+        })
+      );
+  }
+}

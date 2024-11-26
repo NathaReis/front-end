3a2b487c24eff26df34a87fa679228492b81c7e9
frederico.junior <frederico.junior@siriusgroup.com.br>
2024-11-26 10:20:57 -0300

    ajustes tela inicio

--- src/app/pages/inicio/inicio.component.ts
+++ src/app/pages/inicio/inicio.component.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
-import { MenuComponent } from "../menu/menu.component";
+import { MenuComponent } from '../menu/menu.component';
 import { WebSocketService } from '../../services/web-socket.service';
+import { CardModule } from 'primeng/card';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [MenuComponent],
+  imports: [MenuComponent, CardModule],
   templateUrl: './inicio.component.html',
-  styleUrl: './inicio.component.scss'
+  styleUrl: './inicio.component.scss',
+  providers: [MessageService],
 })
 export class InicioComponent {
+  usuarioLogado: string;
 
+  constructor(
+    private webSocketService: WebSocketService,
+    private messageService: MessageService
+  ) {
+    const userName = localStorage.getItem('userName');
+    this.usuarioLogado = userName ? userName : 'Usuário';
+
+    this.webSocketService.connect();
+    this.webSocketService.getNotifications().subscribe((notification) => {
+      this.messageService.add({
+        severity: 'info',
+        summary: 'Nova Notificação',
+        detail: notification,
+      });
+    });
+  }
 }
